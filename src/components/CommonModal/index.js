20179884ee464a48ac1ae0fd7c267522668cc575
safiulannam <safiulannam24@gmail.com>
2023-09-11 13:45:04 +0700

    add navbar & auth

---
+++ src/components/CommonModal/index.js
@@ -0,0 +1,50 @@
+"use client";
+
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment } from "react";
+
+export default function CommonModal({modalTitle}) {
+    return (
+        <Transition.Root as={Fragment}>
+            <Dialog as="div" className={"relative z-10"}>
+                <Transition.Child
+                    as={Fragment}
+                    enter="ease-in-out duration-900"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in-out duration-500"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                >
+                    <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                </Transition.Child>
+                <div className="fixed inset-0 overflow-hidden">
+                    <div className="absolute inset-0 overflow-hidden">
+                        <div className="fixed inset-y-0 right-0 flex max-w-ful pl-10">
+                            <Transition.Child
+                                as={Fragment}
+                                enter="ease-in-out duration-900"
+                                enterFrom="opacity-0"
+                                enterTo="opacity-100"
+                                leave="ease-in-out duration-500"
+                                leaveFrom="opacity-100"
+                                leaveTo="opacity-0"
+                            >
+                                <Dialog.Panel className={"w-screen max-w-md"}>
+                                    <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
+                                        <div className="flex items-start justify-between">
+                                            <Dialog.Title>{modalTitle}</Dialog.Title>
+                                        </div>
+                                        <div className="mt-8">
+                                            {mainContent}
+                                        </div>
+                                    </div>
+                                </Dialog.Panel>
+                            </Transition.Child>
+                        </div>
+                    </div>
+                </div>
+            </Dialog>
+        </Transition.Root>
+    );
+}
